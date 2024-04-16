a261b775f140f7c1cc252e108a0d270214847f57
Amy Lee <amylee041182@gmail.com>
2024-04-16 09:23:15 -0700

    initial studio 1 push

---
+++ studio1/script.js
@@ -0,0 +1,48 @@
+// 2. implement text when hover and disappear when mouse out
+// 3. not hovering, video opacity is 50% and it is 100% when hovered
+// CSS 
+// body{
+//     background-color: #D65076;
+//   }
+  
+//   .parent, .cover{
+//     height: 100px;
+//     width: 100px;
+//     background-color: #FF6F61;
+//   }
+  
+//   .parent{
+//     position: relative;
+//   }
+  
+//   .cover{
+//     opacity: 0;
+//     position: absolute;
+//     top: 0;
+//   }
+  
+//   .cover:hover{
+//     opacity: 100;
+//   }
+
+// HTML
+{/* <div class="parent">
+  Some text would go in here
+  <div class="cover"> 
+    The hover text would go here
+  </div>
+</div> */}
+// https://stackoverflow.com/questions/72467423/how-to-make-a-text-appear-on-hover-and-another-disappear-using-css
+
+// 4. implement play video when hover and pause when mouseout
+// $(document).ready(function() {
+//     $(".myvideos").on("mouseover", function(event) {
+//       this.play();
+  
+//     }).on('mouseout', function(event) {
+//       this.pause();
+  
+//     });
+//   })
+// https://stackoverflow.com/questions/47381793/html-js-how-to-play-stop-video-on-hover
+
